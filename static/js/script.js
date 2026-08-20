7b30fc47540a650f5100a3952f124d7dce5220f0
sandhyaranidharanikota-lgtm <sandhyaranidharanikota@gmail.com>
2026-08-20 08:57:36 +0000

    architecture

---
+++ static/js/script.js
@@ -0,0 +1,41 @@
+let registerForm = document.getElementById("registerForm")
+if(registerForm){
+    registerForm.addEventListener("submit",function(event){
+        event.preventDefault();
+        console.log(event);
+    let namedocument.getElementById("name").value;
+    let email=Document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let date=document.getElementById("date").value;
+    let male=document.getElementById("male").checked;
+    let female=document.getElementById("female").checked;
+    letcourse=document.querySelector('input[name="course"]:checked').value;
+    let gender="";
+    if(male){
+        gender="male";
+    }else if(female){
+        gender="female";
+    }
+    if(name == "" || email == "" || password == "" || date == "" || gender == "" || course == ""){
+        alert("Pleae fill all the fields");
+    }else{
+        alert("Registration successful");
+    }
+})
+}
+let loginForm = document.getElementById("loginForm")
+if(loginForm){
+    loginForm.addEventListener("submit",function(event)
+    {
+        event.preventDefault();
+        console.log(event);
+        let email=document.getElementById("loginEmail").value;
+        letpasword=document.getElementById("loginPassword").value;
+        if(email == "" || password = ""){
+            alert("Please fill all the fields");
+        }ele{
+                    
+        }
+    }
+)
+}
